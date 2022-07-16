1a2f5e03be2ed0ff836afec1f95c498a449b9407
cann9676 <80699111+cann9676@users.noreply.github.com>
2022-07-15 21:56:23 -0400

    Initial Commit

---
+++ UI Practice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UI Practice
+//
+//  Created by Chelsea Hannah on 7/15/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UI Practice/UI_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  UI_PracticeApp.swift
+//  UI Practice
+//
+//  Created by Chelsea Hannah on 7/15/22.
+//
+
+import SwiftUI
+
+@main
+struct UI_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
