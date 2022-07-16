31502fc1439c591f5da25549956b65262f1ccd53
cann9676 <80699111+cann9676@users.noreply.github.com>
2022-07-15 22:44:39 -0400

    New Commit

--- UI Practice/ContentView.swift
+++ UI Practice/ContentView.swift
@@ -9,13 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Image("Fashion1")
+                .resizable()
+                .scaledToFill()
+            //scales the image further
+                .edgesIgnoringSafeArea(.all)
+            //fills image out to the edge of the phone
+           
+            Text("OURSELVES")
+                .font(.custom("EBGaramond", size: 50))
+                .foregroundColor(.white)
+                .multilineTextAlignment(.center)
+                .padding(.top, -400.0)
+            HStack{
+                
+            }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
     }
 }
